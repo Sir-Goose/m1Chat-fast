dc47324182d71eb9d3f8f2ca826893a2784791fb
Matthew Allison <matthewallisonxyz@gmail.com>
2025-05-07 22:18:46 +0200

    Add login functionality with PIN validation and integrate MudBlazor components

--- m1Chat/m1Chat.Client/Program.cs
+++ m1Chat/m1Chat.Client/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http; 
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+builder.Services.AddHttpClient();  
 
 await builder.Build().RunAsync();
---
+++ m1Chat/m1Chat/Services/Completion.cs
@@ -0,0 +1,1 @@
+namespace m1Chat.Services;
