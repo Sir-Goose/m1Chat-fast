4d0c38cd472e194788d314339474e7df5baf6d17
Matthew Allison <matthewallisonxyz@gmail.com>
2025-07-03 22:01:23 +0200

    refactored the rest of the components to use the code behind files

---
+++ m1Chat/m1Chat.Client/Components/ScrollToBottomChip.razor.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace m1Chat.Client.Components;
+
+public partial class ScrollToBottomChip : ComponentBase
+{
+    [Inject] private IScrollManager ScrollManager { get; set; } = default!;
+
+    private MudScrollToTop _scrollToBottomChip = default!;
+    
+    private async Task ScrollToBottom()
+    {
+        _scrollToBottomChip.Visible = false;
+        await ScrollManager.ScrollToBottomAsync(".chat-container", ScrollBehavior.Auto);
+        _scrollToBottomChip.Visible = false;
+    }
+}
